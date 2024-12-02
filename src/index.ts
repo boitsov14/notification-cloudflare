7ccc9023c995f6c623901370590fec3a525a9871
boitsov14 <86334557+boitsov14@users.noreply.github.com>
2024-12-02 23:28:32 +0900

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,29 @@
-import {Hono} from 'hono'
+import type { IncomingRequestCfProperties } from '@cloudflare/workers-types'
+import { Hono } from 'hono'
 
 type Bindings = {
-    X_API_KEY: string
+  X_API_KEY: string
 }
 
 const app = new Hono<{ Bindings: Bindings }>()
 
 app.onError((err, c) => {
-    console.log(err)
-    return c.json({error: "An unexpected error occurred."}, 500)
+  console.log(err)
+  return c.json({ error: 'An unexpected error occurred.' }, 500)
 })
 
-app.get('/', (c) => {
-    const cf = c.req.raw.cf
+app.get('/', c => {
+  const { cf } = c.req.raw as unknown as { cf?: IncomingRequestCfProperties }
 
-    const geolocation = {
-        country: cf?.country,
-        region: cf?.region,
-        city: cf?.city,
-    }
+  const geolocation = {
+    country: cf?.country,
+    region: cf?.region,
+    city: cf?.city,
+    key: c.env.X_API_KEY,
+    hi: 'Hello',
+  }
 
-    return c.json(geolocation)
+  return c.json(geolocation)
 })
 
 export default app
