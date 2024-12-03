31a613bbda7e3ab7860c0fb444916c3add878e1a
boitsov14 <86334557+boitsov14@users.noreply.github.com>
2024-12-03 22:01:33 +0900

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import type { IncomingRequestCfProperties } from '@cloudflare/workers-types'
 import { Hono } from 'hono'
+import { logger } from 'hono/logger'
 
 type Bindings = {
   readonly X_API_KEY: string
@@ -7,22 +8,28 @@ type Bindings = {
 
 // biome-ignore lint/style/useNamingConvention:
 const app = new Hono<{ Bindings: Bindings }>()
+app.use(logger())
 
 app.onError((err, c) => {
-  console.log(err)
+  console.error(err)
   return c.json({ error: 'An unexpected error occurred.' }, 500)
 })
 
-app.get('/', c => {
+app.get('/', async c => {
   const { cf } = c.req.raw as { cf?: IncomingRequestCfProperties }
+  const { country, region, city } = cf || {}
 
   const geolocation = {
-    country: cf?.country,
-    region: cf?.region,
-    city: cf?.city,
+    country,
+    region,
+    city,
     key: c.env.X_API_KEY,
     hi: 'Hello',
   }
+
+  // throw new Error('An error occurred')
+  // wait 1 seconds
+  await new Promise(resolve => setTimeout(resolve, 1000))
 
   return c.json(geolocation)
 })
