f4b5712a834414f80a49abf2de2f0a52b3c4405e
boitsov14 <86334557+boitsov14@users.noreply.github.com>
2024-12-10 20:56:07 +0900

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,47 @@
-import type { IncomingRequestCfProperties } from '@cloudflare/workers-types'
+import type {
+  IncomingRequestCfProperties,
+  RateLimit,
+} from '@cloudflare/workers-types'
+import { cloudflareRateLimiter } from '@hono-rate-limiter/cloudflare'
 import { Hono } from 'hono'
 import { logger } from 'hono/logger'
 
 type Bindings = {
   readonly DISCORD_URL: string
-  // biome-ignore lint/suspicious/noExplicitAny:
-  readonly RATE_LIMITER: any
+  readonly RATE_LIMITER: RateLimit
 }
 
 // biome-ignore lint/style/useNamingConvention:
 const app = new Hono<{ Bindings: Bindings }>()
 
+// log requests
 app.use(logger())
 
+// handle errors
 app.onError((err, c) => {
   console.error(err)
   return c.text('An unexpected error occurred.', 500)
 })
 
-app.post('/', async c => {
-  // rate limit
-  const { success } = await c.env.RATE_LIMITER.limit({ key: c.env.DISCORD_URL })
-  if (!success) {
-    return c.text('Rate limit exceeded', 429)
-  }
+// rate limiter
+app.use(
+  // biome-ignore lint/style/useNamingConvention:
+  cloudflareRateLimiter<{ Bindings: Bindings }>({
+    rateLimitBinding: c => c.env.RATE_LIMITER,
+    keyGenerator: () => '',
+  }),
+)
 
-  // get body
-  const { source, message } = await c.req.json()
+app.post('/', async c => {
+  // get message
+  const { message } = await c.req.json()
 
   // get geolocation
   const { cf } = c.req.raw as { cf?: IncomingRequestCfProperties }
   const { country, region, city } = cf || {}
 
   // set content
-  const content =
-    `${source}\n${country} ${region} ${city}\n${message}`.substring(0, 1000)
+  const content = `${country} ${region} ${city}\n${message}`.substring(0, 1000)
 
   // send to discord
   // todo
