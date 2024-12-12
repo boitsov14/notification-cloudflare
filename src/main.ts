4cf855b76d7d38a50a1b3429cb678b0b6a2132c5
boitsov14 <86334557+boitsov14@users.noreply.github.com>
2024-12-12 21:46:39 +0900

    wip

--- src/main.ts
+++ src/main.ts
@@ -2,37 +2,37 @@ import type { RateLimit } from '@cloudflare/workers-types'
 import { cloudflareRateLimiter } from '@hono-rate-limiter/cloudflare'
 import { Hono } from 'hono'
 import { GeoMiddleware, getGeo } from 'hono-geo-middleware'
+import { env } from 'hono/adapter'
 import { bodyLimit } from 'hono/body-limit'
 import { logger } from 'hono/logger'
 import ky from 'ky'
 import { z } from 'zod'
 
-type Bindings = {
+type Env = {
   readonly DISCORD_URL: string
   readonly RATE_LIMITER: RateLimit
 }
 
-// biome-ignore lint/style/useNamingConvention:
-const app = new Hono<{ Bindings: Bindings }>()
-
+const app = new Hono()
 // log requests
 app.use(logger())
-
 // handle errors
-app.onError((err, c) => {
+app.onError(async (err, c) => {
   console.error(err)
-  return c.text('An unexpected error occurred.', 500)
+  const text = 'An unexpected error occurred: Could not send to Discord.'
+  await ky.post(env<Env>(c).DISCORD_URL, {
+    json: { content: `@everyone\n${text}` },
+    throwHttpErrors: false,
+  })
+  return c.text(text, 500)
 })
-
 // rate limiter
 app.use(
-  // biome-ignore lint/style/useNamingConvention:
-  cloudflareRateLimiter<{ Bindings: Bindings }>({
-    rateLimitBinding: c => c.env.RATE_LIMITER,
+  cloudflareRateLimiter({
+    rateLimitBinding: c => env<Env>(c).RATE_LIMITER,
     keyGenerator: () => '',
   }),
 )
-
 // get geolocation
 app.use('/*', GeoMiddleware())
 
@@ -47,7 +47,7 @@ app.post('/text', async c => {
   // log content
   console.info(content)
   // send to discord
-  await ky.post(c.env.DISCORD_URL, { json: { content: content } })
+  await ky.post(env<Env>(c).DISCORD_URL, { json: { content: content } })
   // return 200
   return c.text('ok')
 })
@@ -56,9 +56,11 @@ app.post('/file', bodyLimit({ maxSize: 8 * 1024 * 1024 }), async c => {
   // get multipart data
   const body = await c.req.parseBody()
   // get file
-  // biome-ignore lint/complexity/useLiteralKeys:
-  const file = body['file'] as File
-  z.instanceof(File).parse(file)
+  const { file } = z.object({ file: z.instanceof(File) }).parse(body)
+  // if file is tex
+  if (file.name.endsWith('.tex')) {
+    // TODO: compile tex
+  }
   // get geolocation
   const { countryCode, region, city } = getGeo(c)
   // set content
@@ -79,13 +81,9 @@ app.post('/file', bodyLimit({ maxSize: 8 * 1024 * 1024 }), async c => {
   // attach file
   formData.append('file', blob, file.name)
   // send to discord
-  await ky.post(c.env.DISCORD_URL, { body: formData })
+  await ky.post(env<Env>(c).DISCORD_URL, { body: formData })
   // return 200
   return c.text('ok')
 })
-
-// TODO: add more routes
-// path: /latex
-// get latex code and render it to png and send it to discord
 
 export default app
